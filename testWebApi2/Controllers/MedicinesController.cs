3a6bdb80ff341f8f9212846d9ee278b886212047
Andrew Munkacsi <liri2006@yandex.ru>
2014-06-07 11:21:49 +0200

    project added

---
+++ testWebApi2/Controllers/MedicinesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using testWebApi2.Models;
+
+namespace testWebApi2.Controllers
+{
+    public class MedicinesController : ApiController
+    {
+        private IEnumerable<Medicine> medicines = new List<Medicine>
+        {
+            new Medicine
+            {
+                Id = 1,
+                Name = "Viagra"
+            },
+            new Medicine
+            {
+                Id = 2,
+                Name = "Ibuprom"
+            }
+        };
+
+        public IEnumerable<Medicine> GetMedicines()
+        {
+            return medicines.ToList();
+        }
+    }
+}
---
+++ testWebApi2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(testWebApi2.Startup))]
+namespace testWebApi2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
